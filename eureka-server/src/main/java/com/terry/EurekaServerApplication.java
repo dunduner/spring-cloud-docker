e84e8a5f6eeede301fd09cdcb2cedfc53a8b0ff7
dunduner <253013263@qq.com>
2019-12-03 15:48:58 +0800

    Update EurekaServerApplication.java

--- eureka-server/src/main/java/com/terry/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/terry/EurekaServerApplication.java
@@ -3,12 +3,20 @@ package com.terry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public  String hello(){
+		return "hello world";
 	}
 }
